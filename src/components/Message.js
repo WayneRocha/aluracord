51c78ea1746f0cae76a24a3906d2cdc119e97d48
Wayne Rocha <dev.waynerocha@gmail.com>
2022-01-29 22:05:32 -0300

    <built> profile card appears when click in a photo of chat

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,12 @@
 import { Box, Text, Image, Icon } from '@skynexui/components';
+import { fetchUser } from '../../services/githubAPI';
+import GithubProfileCard from '../components/GithubProfileCard';
 import ReactMarkdown from 'react-markdown';
 import React, { useState } from 'react';
 import appConfig from '../../config.json';
 import markdownCSS from '../styleSheets/markdown';
 
-function getMessageContentComponent(type, data){
+function getMessageContentComponent(type, data) {
     const types = {
         'message': content => {
             return (
@@ -22,10 +24,10 @@ function getMessageContentComponent(type, data){
                 </>
             );
         },
-        'sticker': ({url}) => {
+        'sticker': ({ url }) => {
             return <Image src={url}></Image>
         },
-        'photo': ({url}) => {
+        'photo': ({ url }) => {
             return <Image src={url}></Image>
         },
         'document': () => {
@@ -37,7 +39,20 @@ function getMessageContentComponent(type, data){
 }
 
 export default function Message(props) {
-    const { 
+    const [wrapCard, setWrapCard] = useState(false);
+    const [githubProfile, setGithubProfile] = useState({});
+    const [messageHover, setmessageHover] = useState(false);
+    const handleClick = async() => {
+        if (wrapCard) {
+            setGithubProfile({});
+            setWrapCard(!wrapCard);
+            return;
+        }
+        const profile = await fetchUser(from);
+        setGithubProfile(profile);
+        setWrapCard(!wrapCard);
+    }
+    const {
         id,
         content,
         messageType,
@@ -46,71 +61,99 @@ export default function Message(props) {
         isMine,
         onDelete
     } = props;
-    const [messageHover, setmessageHover] = useState(false);
-
+    
     return (
-        <Text
-            tag="li"
-            styleSheet={{
-                borderRadius: '5px',
-                padding: '6px',
-                marginBottom: '12px',
-                hover: {
-                    backgroundColor: appConfig.theme.colors.neutrals[700],
-                }
-            }}
-            onMouseOver={() => setmessageHover(true)}
-            onMouseLeave={() => setmessageHover(false)}
-        >
-            <Box
+        <>
+            <Text
+                tag="li"
                 styleSheet={{
-                    display: 'flex',
-                    marginBottom: '8px',
+                    borderRadius: '5px',
+                    padding: '6px',
+                    marginBottom: '12px',
+                    hover: {
+                        backgroundColor: appConfig.theme.colors.neutrals[700],
+                    }
                 }}
+                onMouseOver={() => setmessageHover(true)}
+                onMouseLeave={() => setmessageHover(false)}
             >
-                <Image
+                <Box
                     styleSheet={{
-                        width: '35px',
-                        height: '35px',
-                        borderRadius: '50%',
-                        display: 'inline-block',
-                        marginRight: '8px',
+                        display: 'flex',
+                        marginBottom: '8px',
                     }}
-                    src={`https://github.com/${from}.png`}
-                />
-                <Text tag="strong">
-                    {from}
-                </Text>
-                <Text
-                    styleSheet={{
-                        fontSize: '10px',
-                        marginLeft: '8px',
-                        color: appConfig.theme.colors.neutrals[300],
-                    }}
-                    tag="span"
                 >
-                    {(new Date(timestamp).toLocaleDateString())}
-                </Text>
-                
-                {(isMine && content != '*`Mensagem Deletada`*') && (
-                    <Icon 
-                        name='FaPlus'
+                    <Image
                         styleSheet={{
-                            marginLeft: 'auto',
-                            crossAxisAlignment: 'flex-start',
-                            transform: 'rotate(45deg)',
-                            color: (messageHover) ? appConfig.theme.colors.neutrals['400'] : 'transparent',
+                            width: '35px',
+                            height: '35px',
+                            borderRadius: '50%',
+                            display: 'inline-block',
+                            marginRight: '8px',
                             hover: {
-                                color: appConfig.theme.colors.neutrals['300'],
-                                cursor: 'pointer'
+                                filter: 'brightness(.4)',
                             }
                         }}
-                        onClick={()=> onDelete(id)}/>
+                        src={`https://github.com/${from}.png`}
+                        onMouseOver={()=>{}}
+                        onMouseLeave={()=>{}}
+                        onClick={handleClick}
+                    />
+                    <Text tag="strong">
+                        {from}
+                    </Text>
+                    <Text
+                        styleSheet={{
+                            fontSize: '10px',
+                            marginLeft: '8px',
+                            color: appConfig.theme.colors.neutrals[300],
+                        }}
+                        tag="span"
+                    >
+                        {(new Date(timestamp).toLocaleDateString())}
+                    </Text>
+
+                    {(isMine && content != '*`Mensagem Deletada`*') && (
+                        <Icon
+                            name='FaPlus'
+                            styleSheet={{
+                                marginLeft: 'auto',
+                                crossAxisAlignment: 'flex-start',
+                                transform: 'rotate(45deg)',
+                                color: (messageHover) ? appConfig.theme.colors.neutrals['400'] : 'transparent',
+                                hover: {
+                                    color: appConfig.theme.colors.neutrals['300'],
+                                    cursor: 'pointer'
+                                }
+                            }}
+                            onClick={() => onDelete(id)} />
                     )}
-            </Box>
+                </Box>
 
-            { getMessageContentComponent(messageType, content) }
+                {getMessageContentComponent(messageType, content)}
 
-        </Text>
+            </Text>
+            {wrapCard && (
+                <Box styleSheet={{display: 'absolute', top: 0, left: 0}}>
+                    <Box
+                        styleSheet={{
+                            padding: '16px',
+                            backgroundColor: appConfig.theme.colors.neutrals[800],
+                            border: '1px solid',
+                            borderColor: appConfig.theme.colors.neutrals[999],
+                            borderRadius: '10px',
+                            minHeight: 'min-content',
+                            minWidth: 'min-content',
+                            maxWidth: '30%',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                        }}
+                        >
+                        <GithubProfileCard profileObj={githubProfile}/>
+                    </Box>
+                </Box>
+            )}
+        </>
     );
 }
