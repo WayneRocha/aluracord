a193151388b7332990ff111d9d99fdcb2a97d627
Wayne Rocha <dev.waynerocha@gmail.com>
2022-01-29 21:54:04 -0300

    <style> custom scroll bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import appConfig from '../config.json';
+
 function GlobalStyle() {
     return (
         <style global jsx>{`
@@ -21,6 +23,19 @@ function GlobalStyle() {
         }
         #__next > * {
           flex: 1;
+        }
+
+        ::-webkit-scrollbar {
+          width: 12px;               /* width of the entire scrollbar */
+        }
+
+        ::-webkit-scrollbar-track {
+          background: ${appConfig.theme.colors.neutrals["800"]};        /* color of the tracking area */
+        }
+
+        ::-webkit-scrollbar-thumb {
+          background-color: ${appConfig.theme.colors.neutrals["400"]};    /* color of the scroll thumb */
+          border-radius: 24px;       /* roundness of the scroll thumb */
         }
         /* ./App fit Height */ 
       `}</style>
